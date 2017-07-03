f128820d25464fda461b7fbf1c14e4894fa20e55
jeoeveren <jeroenvanoeveren@hotmail.com>
2017-07-03 14:34:47 +0200

    nav bar and homepg

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+
+# config/routes.rb
+
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
