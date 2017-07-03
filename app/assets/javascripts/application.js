b68570787072d99c9ed08fe17699639c953deeac
jeoeveren <jeroenvanoeveren@hotmail.com>
2017-07-03 12:36:49 +0200

    installed Bootstrap JQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,11 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+// application.js
+
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
